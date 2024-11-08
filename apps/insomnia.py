fcc3a9e8f076a6415b92aa070bc22ddc7385d121
HenriHTTP <zegordoplays123@gmail.com>
2024-11-07 23:21:56 -0300

    refactor: implement serialize install packages

--- apps/insomnia.py
+++ apps/insomnia.py
@@ -1,19 +1,18 @@
-from utils.handle_install import install_package
+from utils.handle_install import serialize_install_packages
 from utils.handle_install import is_package_installed
 
 
 def install_insomnia() -> None:
     if is_package_installed("insomnia"):
         return
-    curl_query:dict[str,str]={
-        "Download insomnia":"wget https://github.com/Kong/insomnia/releases/download/core%4010.1.1/Insomnia.Core-10.1.1.deb -O insomnia.deb",
-        "Install MongoDB Compass package":"sudo dpkg -i insomnia.deb",
-        "Remove .deb file after installation": "rm insomnia.deb"
+    curl_query: dict[str, str] = {
+        "Download insomnia": "wget https://github.com/Kong/insomnia/releases/download/core%4010.1.1/Insomnia.Core-10.1.1.deb -O insomnia.deb",
+        "Install MongoDB Compass package": "sudo dpkg -i insomnia.deb",
+        "Remove .deb file after installation": "rm insomnia.deb",
     }
-    for package_name, query in curl_query.items():
-        try:
-            install_package(query,package_name)
-        except Exception as error:
-            print(f"Error: Failed to install Insomnia: {error}")
-            return
+    try:
+        serialize_install_packages(curl_query)
+    except Exception as error:
+        print(f"{error}")
+        return
     return
